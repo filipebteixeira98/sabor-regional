f14039fbd14df902674ed764c3ecb2b0796a2df2
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-10-19 19:30:02 -0300

    chore: add shadow to header on user scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,13 @@ const linkAction = () => {
 };
 
 navLink.forEach((link) => link.addEventListener('click', linkAction));
+
+const shadowHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 50
+    ? header.classList.add('shadow-header')
+    : header.classList.remove('shadow-header');
+};
+
+window.addEventListener('scroll', shadowHeader);
