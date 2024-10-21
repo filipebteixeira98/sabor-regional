37dd54d959b24d3f081c012a67e036f592f953c5
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2024-10-21 20:45:43 -0300

    chore: add function to show active section option

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,26 @@ const scrollUp = () => {
 };
 
 window.addEventListener('scroll', scrollUp);
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute('id'),
+      sectionsClass = document.querySelector(
+        '.nav__menu a[href*=' + sectionId + ']'
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sections.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
